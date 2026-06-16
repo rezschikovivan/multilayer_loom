ff269420f241f4a4eec7f39dc8d89a6d0b370408
rezschikovivan <rezschikovivan@gmail.com>
2026-06-16 21:40:27 +0300

    Изменил лого и работу в разных ОС

--- loom/view/window.py
+++ loom/view/window.py
@@ -1,4 +1,5 @@
-from tkinter import BOTH, RIGHT, SOLID, Tk
+from tkinter import BOTH, RIGHT, SOLID, Tk, PhotoImage
+from platform import system
 
 from loom.view.tab_menu import TabMenu
 from loom.view.params_panel import ParametrsPanel
@@ -23,13 +24,20 @@ class Window():
     def config_grid(self):
         for r in range(10):self.root.columnconfigure(index=r,weight=1)
         for r in range(10):self.root.rowconfigure(index=r,weight=1)
+
     def config_window(self):
         """Adjusts the window size, title, and icon"""
         self.root.title("Многослойный ткацкий станок КГУ")
-        #self.root.iconbitmap(default="icon.ico", bitmap="icon.ico")# On linux doesn`t work
-        w = self.root.winfo_screenwidth()
-        h = self.root.winfo_screenheight()
-        self.root.geometry(f"{w}x{h}")# set fullscreen size
+        icon = PhotoImage(file="icon.png")
+        self.root.iconphoto(True, icon)
+
+        platform = system()
+        if platform == "Windows":
+            self.root.state("zoomed")
+        if platform == "Linux":
+            w = self.root.winfo_screenwidth()
+            h = self.root.winfo_screenheight()
+            self.root.geometry(f"{w}x{h}")# set fullscreen size
 
     def bind_Z_Y_btns(self):
         """Bind undo and reverse undo buttons"""
