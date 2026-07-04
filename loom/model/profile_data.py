f65e5c1109f5e0a9c075ee7d9cdc93575d43ec3c
rezschikovivan <rezschikovivan@gmail.com>
2026-07-04 18:30:17 +0300

    Коммит с пк на ноут

--- loom/model/profile_data.py
+++ loom/model/profile_data.py
@@ -1,51 +1,101 @@
 from tkinter import IntVar
 from abc import ABC, abstractmethod
 
-class ClickArea:
-    def __init__(self):
-        ...
+class Observer(ABC):
+    @abstractmethod
+    def notify(sekf, grid:WeftsGrid):
+        pass
 
-class TextileElement(ABC):
-    """Интерфейс для любого элемента ткани: составного и индивидуального"""
+class Subject:
     def __init__(self):
-        super().__init__()
+        self.observers: list[Observer] = []
+    def register_observer(self, o: Observer):
+        self.observers.append(o)
+    def remove_observer(self, o: Observer):
+        self.observers.remove(o)
+    def notify_observers(self, grid:WeftsGrid):
+        for o in self.observers:
+            o.notify(grid)
 
-class Weft(TextileElement):
+class Weft:
     """Утóк"""
-    def __init__(self, is_active=True):
+    def __init__(self, is_active:bool):
         self.is_active = is_active
-
-class Warp(TextileElement):
-    """Основа"""
-    def __init__(self, stoke_number:int):
-        self.anchor_points = []
-        self.stoke_number: int = stoke_number
-    def set_anchor(self, point:ClickArea):
-        ...
+        
+class WeftsGrid(Subject):
+    def __init__(self, rows:int=2, columns:int=2):
+        self.wefts = [[Weft(True) for _ in range(columns)]]*rows
+        super().__init__()
+    @property
+    def columns_lenght(self)->int:
+        return self.wefts[0].__len__()
     
-class CompositeTextileElement(TextileElement):
-    @abstractmethod
-    def __init__(self): super().__init__()
-    @abstractmethod
-    def add_element(self, element: "TextileElement"): pass
-    @abstractmethod
-    def remove_element(self, element:"TextileElement"): pass
+    def set_active(self, row:int, column:int):...
+    def set_inactive(self, row:int, column:int):...
 
-class Reed(CompositeTextileElement):
+    def increment_column(self):
+        self.wefts.append([Weft(True)*self.columns_lenght]) 
+        self.notify_observers(self)
+    def decrement_column(self):
+        if self.columns_lenght > 1:
+            for row in self.wefts:
+                
+            self.wefts.pop()
+            self.notify_observers(self)
+    def increment_row(self):
+        self.rows += 1
+        self.notify_observers(self)
+    def decrement_row(self):
+        if self.rows > 1:
+            self.rows -= 1
+            self.notify_observers(self)
+
+
+
+class Warp():
+    """Основа"""
+    def __init__(self, stroke_number:int):
+        self.stroke_number: int = stroke_number
+        self.anchor_points = []
+    def set_anchor(self, point):
+        ...
+
+
+class Reed():
     """Бедро, столбец утков (weft), составной элемент"""
-    def __init__(self):
-        self.wefts = []
-    def add_element(self, weft:Weft):
-        self.wefts.append(weft)
-    def remove_element(self, weft:Weft):
-        self.wefts.remove(weft)
+    def __init__(self, grid:WeftsGrid):
+        grid.register_observer(self)
 
-class FabricGrid(CompositeTextileElement):
-    """Корневой составной обьект двумерного представления ткани"""
-    def __init__(self):
-        self.reeds = []
-    def add_element(self, reed:Reed):
-        self.reeds.append(reed)
-    def remove_element(self, reed:Reed):
-        self.reeds.remove(reed)
+    def notify(sekf, grid:WeftsGrid): ...
 
+    def __init__(self, height:int):
+        self._wefts = []
+        self._click_areas = []
+        for _ in range(height):
+            self.add_weft(Weft())
+
+    def add_weft(self):
+        self._wefts.append(Weft())
+        
+    def remove_weft(self, weft:Weft=None):
+        if weft is None: self._wefts.pop()
+        else: self._wefts.remove(weft)
+
+# class FabricGrid():
+#     """Корневой составной обьект двумерного представления ткани"""
+#     def __init__(self, height:int=2, width:int=2):
+#         self.reeds = []
+#         self.create_grid(height, width)
+
+#     def create_grid(self, height, width):
+#         for _ in range(width):
+#             self.add(Reed(height))
+
+#     def add(self, reed:Reed):
+#         self.reeds.append(reed)
+#     def remove(self, reed:Reed):
+#         self.reeds.remove(reed)
+
+a = WeftsGrid()
+
+print(1)
