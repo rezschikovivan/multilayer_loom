82a2e97f22b47feaf741cbcaefe311ec210114a8
rezschikovivan <rezschikovivan@gmail.com>
2026-08-21 12:39:10 +0300

    Отчистил ветку ассетов

--- loom/model/__init__.py
+++
@@ -1,5 +0,0 @@
-from loom.controller.memo import Memento, Originator
-from loom.model.front import FabricProfile, Side, WeftsGrid
-from loom.model.model_bases import Observer
-
-__all__ = [FabricProfile, Side, Observer, WeftsGrid, Memento, Originator]
--- main.py
+++
@@ -1,7 +0,0 @@
-from loom.model import FabricProfile
-from loom.view.window import Window
-
-fabric = FabricProfile(None)
-win = Window(fabric)
-
-win.run()
